ce8aeba9286eb6acdcc7b48d9bd37c525065bf78
Ayah kodjo  Ayitey <67638556+AyahAyitey@users.noreply.github.com>
2026-09-02 13:36:47 +0000

    Update getdates.js

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,1 +1,12 @@
+// Dynamically set the copyright year in the footer
+const yearSpan = document.getElementById("currentyear");
+if (yearSpan) {
+  yearSpan.textContent = new Date().getFullYear();
+}
+
+// Dynamically display the last modified date of the document
+const lastModifiedPara = document.getElementById("lastModified");
+if (lastModifiedPara) {
+  lastModifiedPara.textContent = "Last Modified: " + document.lastModified;
+}
 
